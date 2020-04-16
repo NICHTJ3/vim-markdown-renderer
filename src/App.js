76f78d5d436469f8e3b06f0c2b96da4ab316bb70
NICHTJ3 <nicholsontrent@gmail.com>
2020-04-17 03:05:35 +1200

    Create markdown previewer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Editor from 'react-codemirror';
+import Markdown from 'react-markdown';
+import 'codemirror/lib/codemirror.css';
 
 function App() {
+  const [md, setMd] = useState('# Put your markdown here');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 className="title">Markdown preview</h1>
+        <Editor
+          value={md}
+          onChange={setMd}
+          autoFocus={true}
+          className="editor"
+          options={{
+            mode: 'markdown',
+            readOnly: false,
+            lineNumbers: true,
+          }}
+        />
+        <Markdown className="preview" source={md} escapeHtml={true} />
+      </div>
+    </>
   );
 }
 
