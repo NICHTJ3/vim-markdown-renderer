84b4b23fc7f6a485ed8c76d465604e806770885f
Trent Nicholson <39316919+NICHTJ3@users.noreply.github.com>
2020-07-03 01:30:14 +1200

    Add react-syntax-highlighter and custom code block rendering (#16)

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from 'react';
 import Markdown from 'react-markdown';
+import CodeBlock from '../CodeBlock';
 import './markdown.css';
 import './preview.css';
 
-export default function Preview({md}) {
+export default function Preview({ md }) {
   return (
     <div>
-        <Markdown className="preview" source={md} escapeHtml={true} />
+      <Markdown
+        className="preview"
+        source={md}
+        renderers={{
+          code: CodeBlock,
+        }}
+        escapeHtml={true}
+      />
     </div>
-  )
+  );
 }
-
